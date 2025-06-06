74c6ffa981d93c8e013518f02696b5f0c00999c0
Dimitris Serafeimidis <135410825+SerDimitris@users.noreply.github.com>
2025-06-06 10:57:13 +0300

    06/06/2025 2nd commit

---
+++ AbsoluteDifferenceWithTripleForGreater/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Write a C# Sharp program to get the absolute difference
+ * between n and 51. If n is broader than 51 return triple
+ * the absolute difference.
+ * 
+ * Sample Input:
+ * 53
+ * 30
+ * 51
+ * 
+ * Expected Output:
+ * 6
+ * 21
+ * 0
+ */
+
+namespace AbsoluteDifferenceWithTripleForGreater
+{
+    internal class Program
+    {
+        public static int AbsDiff(int x)
+        {
+            return x > 51 ? Math.Abs(x - 51) * 3 : Math.Abs(x - 51);
+        }
+        static void Main(string[] args)
+        {
+            int[] input = { 53, 30, 51 };
+
+            for (int i = 0; i < input.Length; i++) Console.WriteLine(AbsDiff(input[i]));
+        }
+    }
+}
